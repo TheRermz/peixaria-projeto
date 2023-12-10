389e218c4253197bd1d295b24015df2bbfd956e0
Murilo Fischer <mufischer@hotmail.com>
2023-12-09 22:53:01 -0300

    Refactor FishController.js

--- backend/controller/FishController.js
+++ backend/controller/FishController.js
@@ -2,20 +2,58 @@ const Fish = require("../model/Fish");
 
 // Create a new fish
 const createFish = async (req, res) => {
-    // const { name, price, description, image, category, quantity } = req.body;
+    //request jwt token
+
+    const { name, price, description, image, category, quantity } = req.body;
+    try {
+        const newFish = await Fish.create({
+            name,
+            price,
+            description,
+            image,
+            category,
+            quantity,
+        });
+        res.status(201).json(newFish);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+// Get all fishes
+const getAllFishes = async (req, res) => {
+    res.send("getAll");
     // try {
-    //     const newFish = await Fish.create({
-    //         name,
-    //         price,
-    //         description,
-    //         image,
-    //         category,
-    //         quantity,
-    //     });
-    //     res.status(201).json(newFish);
+    //     const fishes = await Fish.find();
+
+    //     res.status(200).json(fishes);
     // } catch (error) {
     //     res.status(500).json({ error: error.message });
     // }
+};
+
+// Get a fish by id
+const getFishById = async (req, res) => {
+    res.send("getById");
+    // const { id } = req.params;
+
+    // try {
+    //     const fish = await Fish.findById(id);
+
+    //     if (fish) {
+    //         return res.status(200).json(fish);
+    //     }
+
+    //     res.status(404).json({ message: "Fish not found!" });
+    // } catch (error) {
+    //     res.status(500).json({ error: error.message });
+    // }
+};
+
+// Update a fish by id
+
+const updateFishById = async (req, res) => {
+    res.send("update");
 };
 
 module.exports = {
