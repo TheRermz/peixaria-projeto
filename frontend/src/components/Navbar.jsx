3c5c31fbae002d14d24b081706b69a8fca973435
Murilo Fischer <mufischer@hotmail.com>
2023-12-11 08:11:02 -0300

    Add Navbar component and related styles

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <div className="nav-wrapper">
+        <ul id="nav-mobile" className="left">
+          <li>
+            <h3>
+              <Link to="/">Home</Link>
+            </h3>
+          </li>
+          <li>
+            <h3>
+              <Link to="/login">Login</Link>
+            </h3>
+          </li>
+          <li>
+            <h3>
+              <Link to="/register">Registro</Link>
+            </h3>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
