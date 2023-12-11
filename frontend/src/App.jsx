176b2fad9500859c87d32fdc740b4145fa3faf96
Murilo Fischer <mufischer@hotmail.com>
2023-12-11 08:10:49 -0300

    Refactor App component to include routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
 
-function App() {
+//router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Navbar from "./components/Navbar";
+
+const App = () => {
   return (
-    <>
-      <h1>Ola</h1>
-    </>
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
-}
+};
 
 export default App;
