2d82fade6585b039a07456c7cd3ca9556347e6ca
Murilo Fischer <mufischer@hotmail.com>
2023-12-14 16:50:35 -0300

    Add AdminProfile component and its styles

---
+++ frontend/src/pages/AdminProfile/AdminProfile.jsx
@@ -0,0 +1,16 @@
+import "./AdminProfile.css";
+
+const AdminProfile = () => {
+  return (
+    <div className="admin-profile">
+      <h1>Olá Admin</h1>
+      <h2>Escolha uma opção</h2>
+      <button>Editar Perfil</button>
+      <button>Adicionar peixe</button>
+      <button>Editar Peixe</button>
+      <button>Remover Peixe</button>
+    </div>
+  );
+};
+
+export default AdminProfile;
