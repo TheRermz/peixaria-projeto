709435be42b080a87057bc16a9cb31bf4704cbd8
Murilo Fischer <mufischer@hotmail.com>
2023-12-14 13:48:09 -0300

    Refactor authentication logic and update navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,34 @@ import "./App.css";
 //router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+//hooks
+import { useAuth } from "./hooks/useAuth";
+
 import Home from "./pages/Home/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Navbar from "./components/Navbar";
 
 const App = () => {
+  const { auth, loading } = useAuth();
+  if (loading) return <div>Carregando...</div>;
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={auth ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!auth ? <Login /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/register"
+            element={!auth ? <Register /> : <Navigate to="/" />}
+          />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,51 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+//hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useSelector, useDispatch } from "react-redux";
+
 const Navbar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
   return (
     <nav id="navbar">
       <div className="nav-wrapper">
         <ul id="nav-mobile" className="center">
-          <li>
-            <h3>
-              <Link to="/">Home</Link>
-            </h3>
-          </li>
-          <li>
-            <h3>
-              <Link to="/login">Login</Link>
-            </h3>
-          </li>
-          <li>
-            <h3>
-              <Link to="/register">Registro</Link>
-            </h3>
-          </li>
+          {auth ? (
+            <>
+              <li>
+                <h3>
+                  <Link to="/">Home</Link>
+                </h3>
+              </li>
+              {user && (
+                <li>
+                  <h3>
+                    <Link to={`/profile/${user._id}`}>Perfil</Link>
+                  </h3>
+                </li>
+              )}
+              <li>
+                <h3>
+                  <Link to="/logout">Logout</Link>
+                </h3>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <h3>
+                  <Link to="/login">Login</Link>
+                </h3>
+              </li>
+              <li>
+                <h3>
+                  <Link to="/register">Registro</Link>
+                </h3>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
