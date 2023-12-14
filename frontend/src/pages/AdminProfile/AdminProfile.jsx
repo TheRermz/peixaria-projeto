a23b5711825af8f08ebba01698a2f3014b10bed2
Murilo Fischer <mufischer@hotmail.com>
2023-12-14 17:06:08 -0300

    Add list of admin options to AdminProfile

--- frontend/src/pages/AdminProfile/AdminProfile.jsx
+++ frontend/src/pages/AdminProfile/AdminProfile.jsx
@@ -1,14 +1,25 @@
 import "./AdminProfile.css";
+import { Link } from "react-router-dom";
 
 const AdminProfile = () => {
   return (
     <div className="admin-profile">
       <h1>Olá Admin</h1>
       <h2>Escolha uma opção</h2>
-      <button>Editar Perfil</button>
-      <button>Adicionar peixe</button>
-      <button>Editar Peixe</button>
-      <button>Remover Peixe</button>
+      <ul className="list-admin">
+        <li>
+          <Link to={`/profile/edit`}>Editar Perfil</Link>
+        </li>
+        <li>
+          <Link to={`/fish/create`}>Adicionar Peixe</Link>
+        </li>
+        <li>
+          <Link to={`/fish/edit`}>Editar Peixe</Link>
+        </li>
+        <li>
+          <Link to={`/fish/remove`}>Remover Peixe</Link>
+        </li>
+      </ul>
     </div>
   );
 };
